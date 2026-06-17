1cb5e45e8a811c8ee9f7b21a7e220e05d6b0d225
jvrezendem <jrezendemarciano@gmail.com>
2026-06-17 10:44:08 -0300

    feat: criação da tabela de jogadores

--- index.php
+++ index.php
@@ -14,3 +14,48 @@ header("Content-type: text/html; charset=utf-8", true);
         
     </body>
 </html>
+
+<?php
+// Definindo a conexão com o banco de dados
+$con = mysqli_connect($host, $login, $senha, $bd);
+
+// Verificando se a conexão foi estabelecida
+if(!$con){
+    die("Conexão falhou!");
+}
+
+
+//definição dos dados de clube e jogador puxados do bd
+$sqlJogador = "SELECT * FROM jogador ORDER BY id";
+$tabelaJogador = mysqli_query($con, $sqlJogador);
+
+$sqlClube = "SELECT * FROM clube ORDER BY id";
+$tabelaClube = mysqli_query($con, $sqlClube);
+
+//Criação da tabela de jogadores
+
+//Verifica se há dados nas tabelas, se não houver, exibe mensagem de erro
+if(mysqli_num_rows($tabelaJogador) > 0){
+?>
+
+<tr id="nenhumjogador"><td align="center"> Nenhum jogador encontrado. </td></tr>
+<tr id="adicionarJogador"><td align="center"><input type="button" value="Adicionar Jogador"> </td></tr>
+
+<?php
+//Se houver dados, exibe a tabela de jogadores
+}else{
+?>
+
+<tr id="dadosJogadores" bgcolor="grey">
+    <td id="colunaID" align="left"> ID </td>
+    <td id="colunaNome" align="left"> Nome </td>
+    <td id="colunaNacionalidade" align="left"> Nacionalidade </td>
+    <td id="colunaIdade" align="left"> Idade </td>
+    <td id="colunaPosicao" align="left"> Posição </td>
+    <td id="colunaClube" align="left"> Clube </td>
+    <td id="colunaValorMercado" align="left"> Valor de mercado </td>
+</tr>
+
+<?php
+}
+?>
