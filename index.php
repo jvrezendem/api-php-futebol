9d43db1282c53565f578545be41b015735fb2aa0
jvrezendem <jrezendemarciano@gmail.com>
2026-06-17 10:30:41 -0300

    feat: definição da conexão com o banco de dados e dos dados puxados

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// Definindo o tipo de conteúdo da página
+header("Content-type: text/html; charset=utf-8", true);
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Central do Futebol</title>
+    </head>
+    <body>
+        <h1 id="titulo">Página Inicial</h1>
+        <p id="descricao"> Central com dados de clubes, jogadores do futebol mundial</p>
+        
+    </body>
+</html>
